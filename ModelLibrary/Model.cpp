3ab1095c45af90415a96b7346915fb0717d7842b
Kalfa <theo92320@hotmail.fr>
2020-02-17 15:49:32 +0100

    Init library model part

---
+++ ModelLibrary/Model.cpp
@@ -0,0 +1,5 @@
+//
+// Created by theo on 17/02/2020.
+//
+
+#include "Model.h"
---
+++ ModelLibrary/Model.h
@@ -0,0 +1,14 @@
+//
+// Created by theo on 17/02/2020.
+//
+
+#ifndef MACHINELEARNING_LIB_H
+#define MACHINELEARNING_LIB_H
+
+
+class Model {
+
+};
+
+
+#endif //MACHINELEARNING_LIB_H
