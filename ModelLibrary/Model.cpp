dd4ddab05b87d2b6d38bfec842f361500ce921e4
Esteban Boucher <es.boucher@protomail.com>
2020-02-18 14:14:15 +0100

    Structured as in the course specs, added train_classification signature

--- ModelLibrary/Model.cpp
+++ ModelLibrary/Model.cpp
@@ -8,43 +8,47 @@ public:
     Matrix2d mat;
     //Crée un tableau de poid entre -1 et 1 Random ??
     // TOUTE LES FONCTIONS DOIVENT AVOIR LE POINTEUR DU MODEL POUR POUVOIR LE MODIFIER
-    Model(double** entryValue, int weightSize) {
+    void create_model(double** entryValue, int weightSize) {
         /*this->entryValue = (double**) malloc(sizeof(double*)*listsize);
         for(int i =0;i<arraysize;i++){
             this->entryValue[i] = (double*) malloc(sizeof(double*)*arraysize);
         }*/
-        this->weightSize = weightSize+1;
+        this->weightSize = weightSize + 1;
         srand(time(NULL));
         this->weightArray = (double *) malloc(sizeof(int*)*weightSize+1);
         double weight = fRand(-1.0, 1.0);
     }
     //somme pondéré pedict regression
     //sign
-    double predict_regression(double * values) {
+    double predict_regression(double* values) {
         double result = 0;
-        for (int i=0; i<weightSize; i++) {
+        for (int i = 0; i < weightSize; i++) {
             result += values[i] * weightArray[i];
         }
         return result;
     }
 
-    double predict_linear(double * values) {
+    double predict_classification(double* values) {
         //SIGN
-        return sigmoid(predict_regression(values));
+        return sign(predict_regression(values));
     }
 
-    void train(double * dataset, double * expected_output, int sizedataset, double pas) {
+    void train_regression(double *in_dataset, double *expected_outputs, int size_dataset, double pas) {
         // pour X iter
         //  Appliquer regle de rosenblatt
         // k étant un echantillon
-        // W + a(Y^K-g(X^k))X^
+        // W + a(Y^K-g(X^k))X^k
         // V4 + (double * V4)
         //          V4
-        for(int i=0; i<sizedataset; i++) {
+        for(int i = 0; i < size_dataset; i++) {
             weightArray = (pas*(dataset[i]) - predict_regression(dataset[i])) * dataset[i];
             predict_regression(dataset[i]);
         }
-        Map<MatrixXd> X=33;
+        Map<MatrixXd> X = 33;
+    }
+
+    void train_classification(double *in_dataset, double *expected_outputs, int alpha, int nb_iter, int previous_weight) {
+
     }
 
     double fRand(double fMin, double fMax) {
@@ -52,7 +56,7 @@ public:
         return fMin + f * (fMax - fMin);
     }
 
-    double sigmoid(double x) {
+    double sign(double x) {
         return 1.0 / (1.0 + exp(-x));
     }
 };
--- ModelLibrary/Model.h
+++ ModelLibrary/Model.h
@@ -16,11 +16,13 @@ public:
     int weightSize;
     Matrix2d mat;
 
-    Model(double** entryValue, int weightSize);
+    void create_model(double** entryValue, int weightSize);
     double predict_regression(double * values);
     double predict_linear(double * values);
-    void train(double * dataset, double * expected_output, int sizedataset, double pas);
+    void train_regression(double *in_dataset, double *expected_outputs, int size_dataset, double pas);
+    void train_classification(double *in_dataset, double *expected_outputs, int alpha, int nb_iter, int previous_weight);
     double fRand(double fMin, double fMax);
+    double sign(double x);
 }
 
 #endif MODEL_H
